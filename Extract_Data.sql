30049a213d7f24a4601911f1d1bbc2a0ce252a6c
led-ray <151374279+led-ray@users.noreply.github.com>
2024-01-08 01:18:28 +0900

    v1.1

--- Extract_Data.sql
+++ Extract_Data.sql
@@ -1,1 +1,69 @@
+-- よく見られているエピソードを知りたいです。エピソード視聴数トップ3のエピソードタイトルと視聴数を取得してください
 
+SELECT 
+    Episodes.Episode_Title AS Title,
+    Episode_Views.Views AS Views
+FROM Episode_Views
+JOIN Episodes ON Episode_Views.Episode_ID = Episodes.Episode_ID
+ORDER BY Episode_Views.Views DESC
+LIMIT 3;
+
+
+-- よく見られているエピソードの番組情報やシーズン情報も合わせて知りたいです。エピソード視聴数トップ3の番組タイトル、シーズン数、エピソード数、エピソードタイトル、視聴数を取得してください
+
+SELECT
+    Programs.Program_Title AS Program,
+    Seasons.Season_Num AS Season,
+    Episodes.Episode_Num AS "Episode Number",
+    Episodes.Episode_Title AS Episode,
+    SUM(Episode_Views.Views) AS Views
+FROM Episode_Views
+JOIN Episodes ON Episode_Views.Episode_ID = Episodes.Episode_ID
+JOIN Seasons ON Episodes.Season_ID = Seasons.Season_ID
+JOIN Programs ON Seasons.Program_ID = Programs.Program_ID
+GROUP BY Programs.Program_ID, Seasons.Season_Num, Episodes.Episode_Num, Episodes.Episode_Title
+ORDER BY SUM(Episode_Views.Views) DESC
+LIMIT 3;
+
+
+-- 本日の番組表を表示するために、本日、どのチャンネルの、何時から、何の番組が放送されるのかを知りたいです。本日放送される全ての番組に対して、
+-- チャンネル名、放送開始時刻(日付+時間)、放送終了時刻、シーズン数、エピソード数、エピソードタイトル、エピソード詳細を取得してください。
+-- なお、番組の開始時刻が本日のものを本日方法される番組とみなすものとします
+
+SELECT
+    Channels.Channel_Name AS Channel,
+    Channel_Timetable.Timetable AS "Start",
+    ADDTIME(Channel_Timetable.Timetable, Episodes.Duration) AS "End",
+    Seasons.Season_Num AS Season,
+    Episodes.Episode_Num AS "Episode Number",
+    Episodes.Episode_Title AS Episode,
+    Episodes.Episode_Desc AS "Description"
+FROM Channel_Timetable
+JOIN Channels ON Channel_Timetable.Channel_ID = Channels.Channel_ID
+JOIN Programs ON Channel_Timetable.Program_ID = Programs.Program_ID
+JOIN Episode_Views ON Channel_Timetable.Timetable = Episode_Views.Timetable
+JOIN Episodes ON Episode_Views.Episode_ID = Episodes.Episode_ID
+JOIN Seasons ON Episodes.Season_ID = Seasons.Season_ID
+WHERE DATE(Channel_Timetable.Timetable) = DATE('2024-01-08'); --テスト用のクエリのため仮の日時を設定しています。実際にはNOW()を使ってください
+
+
+-- ドラマというチャンネルがあったとして、ドラマのチャンネルの番組表を表示するために、本日から一週間分、何日の何時から何の番組が放送されるのかを知りたいです。
+-- ドラマのチャンネルに対して、放送開始時刻、放送終了時刻、シーズン数、エピソード数、エピソードタイトル、エピソード詳細を本日から一週間分取得してください
+
+SELECT 
+    Channels.Channel_Name AS Channel,
+    Channel_Timetable.Timetable AS "Start",
+    ADDTIME(Channel_Timetable.Timetable, Episodes.Duration) AS "End",
+    Seasons.Season_Num AS Season,
+    Episodes.Episode_Num AS "Episode Number",
+    Episodes.Episode_Title AS Episode,
+    Episodes.Episode_Desc AS "Description"
+FROM Channel_Timetable
+JOIN Channels ON Channel_Timetable.Channel_ID = Channels.Channel_ID
+JOIN Programs ON Channel_Timetable.Program_ID = Programs.Program_ID
+JOIN Episode_Views ON Channel_Timetable.Timetable = Episode_Views.Timetable
+JOIN Episodes ON Episode_Views.Episode_ID = Episodes.Episode_ID
+JOIN Seasons ON Episodes.Season_ID = Seasons.Season_ID
+WHERE Channels.Channel_Name = 'Channel 1'--チャンネル名はサンプルデータに合わせて Channel 1 としています。適宜設定してください
+AND DATE(Channel_Timetable.Timetable) BETWEEN DATE('2024-01-02')
+AND DATE_ADD(DATE('2024-01-02'), INTERVAL 7 DAY); --テスト用のクエリのため仮の日時を設定しています。実際にはNOW()を使ってください
--- Make_Sampledata.sql
+++ Make_Sampledata.sql
@@ -1,1 +1,69 @@
+-- Channels
+INSERT INTO Channels (Channel_ID, Channel_Name) VALUES
+(1, 'Channel 1'),
+(2, 'Channel 2'),
+(3, 'Channel 3');
 
+-- Programs
+INSERT INTO Programs (Program_ID, Program_Title, Program_Desc) VALUES
+(1, 'Program 1', 'hoge'),
+(2, 'Program 2', 'huga'),
+(3, 'Program 3', 'hugo');
+
+-- Channel_Timetable
+INSERT INTO Channel_Timetable (Timetable, Channel_ID, Program_ID) VALUES
+('2024-01-01 0:00:00', 1, 1),
+('2024-01-01 1:00:00', 2, 1),
+('2024-01-01 2:00:00', 3, 1),
+('2024-01-08 0:00:00', 1, 2),
+('2024-01-08 1:00:00', 2, 2),
+('2024-01-08 2:00:00', 3, 2),
+('2024-01-15 0:00:00', 1, 3),
+('2024-01-15 1:00:00', 2, 3),
+('2024-01-15 2:00:00', 3, 3);
+
+-- Seasons
+INSERT INTO Seasons (Season_ID, Season_Num, Program_ID) VALUES
+(1, 1, 1),
+(2, 2, 1),
+(3, 1, 2),
+(4, 2, 2),
+(5, 1, 3);
+
+-- Episodes
+INSERT INTO Episodes (Episode_ID, Season_ID, Episode_Num, Episode_Title, Episode_Desc, Duration, Release_Date) VALUES
+(1, 1, 1, 'Episode A', 'A', '00:30:00','2024-01-01 0:00:00'),
+(2, 3, 1, 'Episode B', 'AA', '01:00:00','2024-01-01 1:00:00'),
+(3, 5, 1, 'Episode C', 'AAA', '00:45:00','2024-01-01 2:00:00'),
+(4, 1, 2, 'Episode D', 'AAAA', '00:30:00','2024-01-08 0:00:00'),
+(5, 3, 2, 'Episode E', 'AAAAA', '01:00:00','2024-01-08 1:00:00'),
+(6, 5, 2, 'Episode F', 'AAAAAA', '00:45:00','2024-01-08 2:00:00'),
+(7, 2, 1, 'Episode G', 'AAAAAAA', '00:30:00','2024-01-15 0:00:00'),
+(8, 4, 1, 'Episode H', 'AAAAAAAA', '01:00:00','2024-01-15 1:00:00'),
+(9, 5, 3, 'Episode I', 'AAAAAAAAA', '00:45:00','2024-01-15 2:00:00');
+
+-- Genres
+INSERT INTO Genres (Genre_ID, Genre_Name) VALUES
+(1, 'Genre 1'),
+(2, 'Genre 2'),
+(3, 'Genre 3');
+
+-- Program_Genre
+INSERT INTO Program_Genre (Program_ID, Genre_ID) VALUES
+(1, 1),
+(1, 2),
+(2, 2),
+(2, 3),
+(3, 3);
+
+-- Episode_Views
+INSERT INTO Episode_Views (Views, Channel_ID, Episode_ID, Timetable) VALUES
+(50, 1, 1, '2024-01-01 0:00:00'),
+(100, 2, 2, '2024-01-01 1:00:00'),
+(150, 3, 3, '2024-01-01 2:00:00'),
+(200, 1, 4, '2024-01-08 0:00:00'),
+(250, 2, 5, '2024-01-08 1:00:00'),
+(300, 3, 6, '2024-01-08 2:00:00'),
+(350, 1, 7, '2024-01-15 0:00:00'),
+(400, 2, 8, '2024-01-15 1:00:00'),
+(450, 3, 9, '2024-01-15 2:00:00');
